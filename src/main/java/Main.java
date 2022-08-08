df9e97100d497386a6aa41a957d297301ad87f87
Aleksandr <gmreloj@gmail.com>
2022-08-08 10:54:02 +0300

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int countComp = 600; // кол-во компьютеров
-        int countStep = countComp - 2;
-        int tempResult = 0;
-        int count = 0;
+        int countStep = countComp - 2; // количество шагов
+        int tempResult = 0; //переменная для сложения
+        int count = 0; //переменная для удвоения
 
         for (int i = 0; i < countStep; i++) {
-            tempResult = tempResult + i;
-            count = i;
+            tempResult = tempResult + i; //сложение линий
+            count = i;//получение результата для удвоения
         }
 
         int result = tempResult + count + countComp;
